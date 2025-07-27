f29292f3868ca414850c594c0d7771f05e75fffe
nguy.jenni@gmail.com <nguy.jenni@gmail.com>
2025-07-27 18:36:47 -0500

    Add the total transactions, actual frauds, flagged transactions

--- model.py
+++ model.py
@@ -8,3 +8,11 @@ df = pd.read_csv(dataset_url)
 
 # Clean column names
 df.columns = df.columns.str.strip()
+
+# Total transactions
+total_transactions = len(df)
+
+# Count of actual fraudulent transactions
+actual_frauds = df['Is Fraudulent'].sum()
+# Count of transactions flagged as fraudulent
+flagged_transactions = df['Was Flagged Fraudulent'].sum()
